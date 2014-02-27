76f568bb692f38422da82e25a888e4efbdd9ac48
rodenr <ryan.roden@colorado.edu>
2014-02-27 15:45:34 -0800

    initial commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,21 @@
 class MoviesController < ApplicationController
 
+  def index
+#    @movies = Movie.find(:all).sort { |a,b| a.released_on <=> b.released_on }
+#     @movies = Movie.all
+    @movies_ratings = Movie.ratings.sort
+    session[:sort_by] ||= 'title'
+    session[:ratings] ||= @movies_ratings
+    session[:sort_by] = (params[:sort].blank? ? session[:sort_by] : params[:sort])
+    
+    session[:ratings] = (params[:ratings].blank? ? session[:ratings] : params[:ratings].keys)
+    @movies = Movie.where(:rating => session[:ratings]).order("#{session[:sort_by]} ASC")
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-    @movies = Movie.all
   end
 
   def new
